57b03d74d5cdd78d8af8f93720c61726ddfacc16
Broc Buhler <brocbuhler@gmail.com>
2025-03-03 18:07:01 -0800

    last of Almost Amazon clean up

--- api/authorData.js
+++ api/authorData.js
@@ -83,7 +83,7 @@ const updateAuthor = (payload) => new Promise((resolve, reject) => {
 
 // TODO: GET A SINGLE AUTHOR'S BOOKS
 const getAuthorBooks = (firebaseKey) => new Promise((resolve, reject) => {
-  fetch(`${firebaseEndpoint}/books.json?orderBy="author_id"&equalTo=${firebaseKey}`, {
+  fetch(`${firebaseEndpoint}/books.json?orderBy="author_id"&equalTo="${firebaseKey}"`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
